2c87f8242a88a7b3c48f67af32d51bf1d7b55acf
PyGeoffrey <geoffrey.dai314@gmail.com>
2026-05-21 15:27:19 -0700

    Refactor RDSApp class and add FastAPI routes

--- rds_app.py
+++ rds_app.py
@@ -12,6 +12,7 @@ from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 from fastapi import FastAPI
 
 app = FastAPI()
+@app.get("/")
 class RDSApp:
     """Handles RDS operations with best practices."""
 
